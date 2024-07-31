9253eea193418081f1fe56f55084d3341ec46ed4
kyle <nojaim.k@gmail.com>
2024-07-31 19:19:23 -0400

    local storage stores history, input field cleared after entered

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
-let url = "https://api.collectapi.com/imdb/imdbSearchByName?query=inception"
+// let url = "https://api.collectapi.com/imdb/imdbSearchByName?query=inception"
 
-fetch('https://api.collectapi.com/imdb/imdbSearchByName?query=inception', {
-    method : 'GET',
-    headers: {
-        'content-type' : 'application/json',
-        'authorization' : 'apikey 4w2SNXofae9kR2qbQ4nztb:5ELeaTekWJ1om9OC2morSD'
+
+if (!localStorage.getItem('history')){
+    localStorage.setItem('history', '')
+}
+
+
+let search = ''
+
+document.querySelector('button').addEventListener('click', () => {
+    search = document.getElementById('title').value.toLowerCase()
+    let history = localStorage.getItem('history')
+    let historyArr = history ? history.split(',') : []
+    
+    if (!historyArr.includes(search)){
+        historyArr.unshift(search)
     }
+    
+
+    localStorage.setItem('history', historyArr.join(','))
+    getTitles()
+    document.getElementById('title').value = ''
 })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
+
+
+function getTitles(){
+    fetch(`https://api.collectapi.com/imdb/imdbSearchByName?query=${search}`, {
+        method : 'GET',
+        headers: {
+            'content-type' : 'application/json',
+            'authorization' : 'apikey 4w2SNXofae9kR2qbQ4nztb:5ELeaTekWJ1om9OC2morSD'
+        }
     })
-    .catch(err => {
-        console.log(`the error: ${err} occurred`)
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.result)
+        })
+        .catch(err => {
+            console.log(`the error: ${err} occurred`)
+        })
+}
